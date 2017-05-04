f4a83b2fbcffd6fc475c59b6304a7369421e8d16
DrDarkySoul <Rishat_Valitov@Rishats-MacBook-Pro.local>
2017-05-04 19:00:12 +0300

    Case Duplicates done

--- Duplicates.java
+++ Duplicates.java
@@ -9,11 +9,25 @@ public class Duplicates {
     private final List<Object> bestListEver = new ArrayList();
     
     public void cleareDuplicates() {
-    //...
+        for(Integer i = 0; i < bestListEver.size(); i++)
+            for(Integer j = i + 1; j < bestListEver.size(); j++)
+                if(bestListEver.get(i) == bestListEver.get(j)) {
+                    bestListEver.remove(bestListEver.get(j));
+                    j--;
+                }
     }
     
     public void clearSame() {
-    //...
+        List<Integer> list = bestListEver.stream().distinct().collect(Collectors.toList());
+        Iterator<Integer> i = bestListEver.iterator();
+
+        while (i.hasNext()) {
+            Integer val = i.next();
+            if (list.contains(val))
+                list.remove(val);
+            else
+                i.remove();
+        }
     }
 
     public List<Object> getBestListEver() {
