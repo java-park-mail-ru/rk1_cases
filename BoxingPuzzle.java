5d64f25c94220c81a12c139107d42205eecca72b
wolf1996 <ksgiv37@gmail.com>
2017-05-26 20:14:26 +0300

    fixed

--- BoxingPuzzle.java
+++ BoxingPuzzle.java
@@ -1,7 +1,4 @@
-/**
- * Что здесь не так? Объясните почему? Как можно поправить?
- */
-public class BoxingPuzzle {
+class BoxingPuzzle {
 
     public void puzzleMe() {
         Integer n = 6;
@@ -11,16 +8,16 @@ public class BoxingPuzzle {
         if (n == 6)
             System.out.println("Six little nigger boys playing with a hive;\n" +
                     "A bumble-bee stung one, and then there were five.");
-        decrement(n);
+        n = decrement(n);
         if (n == 5)
-        System.out.println("Five little nigger boys going in for law;\n" +
-                "One got in chancery, and then there were four.");
-        decrement(n);
+            System.out.println("Five little nigger boys going in for law;\n" +
+                    "One got in chancery, and then there were four.");
+        n = decrement(n);
 
         if (n == 4)
-        System.out.println("Four little nigger boys going out to sea;\n" +
-                "A red herring swallowed one, and then there were three.");
-        decrement(n);
+            System.out.println("Four little nigger boys going out to sea;\n" +
+                    "A red herring swallowed one, and then there were three.");
+        n = decrement(n);
 
         if (n == half)
             System.out.println("debug: Only half is left");
@@ -28,17 +25,17 @@ public class BoxingPuzzle {
         if (n == 3)
             System.out.println("Three little nigger boys walking in the Zoo;\n" +
                     "A big bear hugged one, and then there were two.\n");
-        decrement(n);
+        n = decrement(n);
 
         if (n == 2)
-        System.out.println("Two little nigger boys sitting in the sun; \n" +
-                "One got frizzled up, and then there was one.");
-        decrement(n);
+            System.out.println("Two little nigger boys sitting in the sun; \n" +
+                    "One got frizzled up, and then there was one.");
+        n = decrement(n);
 
         if (n == 1)
             System.out.println("One little nigger boy left all alone;\n" +
                     "He went out and hanged himself and then there were None.");
-        decrement(n);
+        n = decrement(n);
 
         if (n == 0)
             n = None;
