3020f36e4b9c29b27405cf47a1c16b72bd937554
akenoq <akenoq@yandex.ru>
2017-10-18 17:32:14 +0300

    rk_1_change Kuklina

--- BoxingPuzzle.java
+++ BoxingPuzzle.java
@@ -1,6 +1,10 @@
 /**
  * Что здесь не так? Объясните почему? Как можно поправить?
+ *
+ * Ссылка передавалась по значению, а когда делаем декремент создается новый обьект и меняется ссылка
+ * Поэтому для изменения ссылки снаружи надо вернуть новую ссылку
  */
+
 public class BoxingPuzzle {
 
     public void puzzleMe() {
@@ -11,16 +15,17 @@ public class BoxingPuzzle {
         if (n == 6)
             System.out.println("Six little nigger boys playing with a hive;\n" +
                     "A bumble-bee stung one, and then there were five.");
-        decrement(n);
+        n = decrement(n);
+
         if (n == 5)
-        System.out.println("Five little nigger boys going in for law;\n" +
-                "One got in chancery, and then there were four.");
-        decrement(n);
+            System.out.println("Five little nigger boys going in for law;\n" +
+                    "One got in chancery, and then there were four.");
+        n = decrement(n);
 
         if (n == 4)
-        System.out.println("Four little nigger boys going out to sea;\n" +
-                "A red herring swallowed one, and then there were three.");
-        decrement(n);
+            System.out.println("Four little nigger boys going out to sea;\n" +
+                    "A red herring swallowed one, and then there were three.");
+        n = decrement(n);
 
         if (n == half)
             System.out.println("debug: Only half is left");
@@ -28,17 +33,17 @@ public class BoxingPuzzle {
         if (n == 3)
             System.out.println("Three little nigger boys walking in the Zoo;\n" +
                     "A big bear hugged one, and then there were two.\n");
-        decrement(n);
+        n = decrement(n);
 
         if (n == 2)
-        System.out.println("Two little nigger boys sitting in the sun; \n" +
-                "One got frizzled up, and then there was one.");
-        decrement(n);
+            System.out.println("Two little nigger boys sitting in the sun; \n" +
+                    "One got frizzled up, and then there was one.");
+        n = decrement(n);
 
         if (n == 1)
             System.out.println("One little nigger boy left all alone;\n" +
                     "He went out and hanged himself and then there were None.");
-        decrement(n);
+        n = decrement(n);
 
         if (n == 0)
             n = None;
@@ -48,7 +53,6 @@ public class BoxingPuzzle {
     }
 
     private Integer decrement(Integer i) {
-        i--;
-        return i;
+        return --i;
     }
 }
